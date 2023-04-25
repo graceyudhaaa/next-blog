a464bbec281e0ad315e77fb6d3874b0e03459280
graceyudhaaa <emmanuelgrace005@gmail.com>
2023-04-25 09:50:42 +0700

    install storybook

--- sanity/schema/post-schema.ts
+++ sanity/schema/post-schema.ts
@@ -1,46 +1,46 @@
 import { Rule } from "sanity"
 
 const post = {
-    title: 'Post',
-    name: 'post',
-    type: 'document',
-    fields: [
-      {
-        name: 'title',
-        title: 'Title',
+  title: 'Post',
+  name: 'post',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: (Rule: Rule) => Rule.required()
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: { source: 'title' },
+      validation: (Rule: Rule) => Rule.required()
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      option: { hotspot: true },
+      fields: [{
+        name: 'alt',
+        title: 'Alt',
         type: 'string',
-        validation: (Rule: Rule) => Rule.required()
-      },
-      {
-        name: 'slug',
-        title: 'Slug',
-        type: 'slug',
-        options: {source: 'title'},
-        validation: (Rule: Rule) => Rule.required()
-      },
-      {
-        name: 'image',
-        title: 'Image',
-        type: 'image',
-        option: {hotspot: true},
-        fields: [{
-            name:'alt',
-            title: 'Alt',
-            type: 'string',
 
-        }]
-      },
-      {
-        name: 'content',
-        title: 'Content',
-        type: 'array',
-        of: [
-          {type: 'block',},
-          {type: 'image',},
-          {type: 'code',},
-        ],
-      },
-    ],
-  }
+      }]
+    },
+    {
+      name: 'content',
+      title: 'Content',
+      type: 'array',
+      of: [
+        { type: 'block', },
+        { type: 'image', },
+        { type: 'code', },
+      ],
+    },
+  ],
+}
 
-  export default post
+export default post
