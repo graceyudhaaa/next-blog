1d02f2284cbe833b40b67942b078b6f94f5b651d
graceyudhaaa <emmanuelgrace005@gmail.com>
2023-04-27 02:52:59 +0700

    remove warning

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -26,10 +26,10 @@ function Navbar() {
       <div>project</div>
       <div>contact</div>
       <label className="relative inline-flex items-center cursor-pointer">
-        <input type="checkbox" value="" checked={theme === 'dark'} className="sr-only peer" />
+        <input type="checkbox" value="" checked={theme === 'dark'} className="sr-only peer" onChange={() => { }} />
         <div className="w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[4px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600" onClick={() => {
           setTheme(theme === 'light' ? 'dark' : 'light')
-        }}></div>
+        }} ></div>
       </label>
     </div>
   </header>)
